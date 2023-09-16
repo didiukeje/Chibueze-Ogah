6ed0924e6fb6aa92b4ed923f65df7ab5c1687e46
Ukeje Patience Ndidiamaka <patienceukeje3006@gmail.com>
2023-09-16 15:23:57 +0100

    Initial commit

---
+++ src/components/BirthdayContent.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+
+function BirthdayContent() {
+    
+}
+export default BirthdayContent;
---
+++ src/components/Login.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { useState, Input } from 'react';
+import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Flex } from '@chakra-ui/react'
+
+
+export const Login = ()=> {
+    const [input, setInput] = useState('')
+  
+    const handleInputChange = (e) => setInput(e.target.value)
+  
+    const isError = input === ''
+    return (
+        <Flex>
+        <FormControl isInvalid={isError}>
+            <FormLabel>Email</FormLabel>
+            <Input type="email" value={input} onChange={handleInputChange}/>
+            {!isError ? (
+                    <FormHelperText>
+                    Enter the special email you were given by Didi.
+                    </FormHelperText>
+                ) : (
+                <FormErrorMessage>Your speacial email is required.</FormErrorMessage>
+            )}
+        </FormControl>
+        </Flex>
+    );
+};
+
