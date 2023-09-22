6afaa2c0a1230e29a5a0708a7d9299c0a0b9bbed
Ukeje Patience Ndidiamaka <patienceukeje3006@gmail.com>
2023-09-22 10:36:15 +0100

    Routing fix

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
 // import React from 'react';
+import { useState, Input } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import BirthdayContent from './components/BirthdayContent';
-import {Login} from './components/Login';
+// import Login from './components/Login';
 // import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route exact path='/' Component={HomePage}/>
-        <Route exact path='/Login' Component={Login}/>
-        <Route exact path='/BirthdayContent' Component={BirthdayContent}/>
-      </Routes>
-    </Router>
+    const [input, setInput] = useState('')
+  
+    const handleInputChange = (e) => setInput(e.target.value)
+  
+    const isError = input === ''
+    return (
+        <Flex>
+        <FormControl isInvalid={isError}>
+            <FormLabel>Email</FormLabel>
+            <Input type="email" value={input} onChange={handleInputChange}/>
+            {!isError ? (
+                    <FormHelperText>
+                    Enter the special email you were given by Didi.
+                    </FormHelperText>
+                ) : (
+                <FormErrorMessage>Your speacial email is required.</FormErrorMessage>
+            )}
+        </FormControl>
+        </Flex>
+    // <Router>
+    //   <Routes>
+    //     <Route exact path='/' element={<HomePage />}/>
+    //     <Route exact path='/Login' element={<Login />}/>
+    //     <Route exact path='/BirthdayContent' element={<BirthdayContent />}/>
+    //   </Routes>
+    // </Router>
   );
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import { useState, Input } from 'react';
-import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Flex } from '@chakra-ui/react'
+// import React from 'react';
+// import { useState, Input } from 'react';
+// import { FormControl, FormLabel, FormErrorMessage, FormHelperText, Flex } from '@chakra-ui/react'
 
 
-export const Login = ()=> {
-    const [input, setInput] = useState('')
+// const Login = ()=> {
+//     const [input, setInput] = useState('')
   
-    const handleInputChange = (e) => setInput(e.target.value)
+//     const handleInputChange = (e) => setInput(e.target.value)
   
-    const isError = input === ''
-    return (
-        <Flex>
-        <FormControl isInvalid={isError}>
-            <FormLabel>Email</FormLabel>
-            <Input type="email" value={input} onChange={handleInputChange}/>
-            {!isError ? (
-                    <FormHelperText>
-                    Enter the special email you were given by Didi.
-                    </FormHelperText>
-                ) : (
-                <FormErrorMessage>Your speacial email is required.</FormErrorMessage>
-            )}
-        </FormControl>
-        </Flex>
-    );
-};
+//     const isError = input === ''
+//     return (
+//         <Flex>
+//         <FormControl isInvalid={isError}>
+//             <FormLabel>Email</FormLabel>
+//             <Input type="email" value={input} onChange={handleInputChange}/>
+//             {!isError ? (
+//                     <FormHelperText>
+//                     Enter the special email you were given by Didi.
+//                     </FormHelperText>
+//                 ) : (
+//                 <FormErrorMessage>Your speacial email is required.</FormErrorMessage>
+//             )}
+//         </FormControl>
+//         </Flex>
+//     );
+// };
 
+// export default Login;
